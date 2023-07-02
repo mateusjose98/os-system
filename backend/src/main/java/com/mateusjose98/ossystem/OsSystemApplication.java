9b0f3dca8a8fb6ae3f843a7a42164f45fba4d970
josemateus <josemateus.ufma@gmail.com>
2023-07-02 10:52:53 -0300

    Project created

---
+++ backend/src/main/java/com/mateusjose98/ossystem/OsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mateusjose98.ossystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsSystemApplication.class, args);
+	}
+
+}
